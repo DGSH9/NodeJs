821838d4b76bf77890b31df91a65d2350305ea2a
Durgesh Kumar Yadav <57389386+DGSH9@users.noreply.github.com>
2022-01-02 16:21:09 +0530

    Assignemt-1 Completed Working with user input

--- assignments/assignment_1/index.js
+++ assignments/assignment_1/index.js
@@ -1,14 +1,21 @@
 function getNameFromCommandLine() {
     // Write you code here, name should be taken as args in process.argv
-    // This is testing
+    const name1 = process.argv;
+    return name1[2]
 }
 
 function getNameFromEnv() {
-    // Write your code here
+    var name2 = process.env.name
+    return name2
 }
 
+const readline = require('readline');
 function getNameFromReadLine() {
-    // Write your code here
+    const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+    rl.question("Please Enter Your Name : ", (ans) => {
+        getNameFromReadLine(ans);
+        rl.close()
+    });
 
 }
 module.exports = {
